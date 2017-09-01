569056357cbe67f6b9e7eb9e5756890115e20d6c
Md Sarfaraz Haque <sarfarazhaq@hotmail.com>
2017-09-01 05:29:15 -0700

    Update TipViewController.swift

--- tippy/TipViewController.swift
+++ tippy/TipViewController.swift
@@ -1,24 +1,50 @@
 //
 //  ViewController.swift
-//  tippy
+//  Tippo
 //
-//  Created by Md Sarfaraz Haque on 8/16/17.
+//  Created by Md Sarfaraz Haque on 9/1/17.
 //  Copyright © 2017 Sarfaraz. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
     
-    @IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var totalLabel: UILabel!
+    
     @IBOutlet weak var billField: UITextField!
     
-    @IBOutlet var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+
+    @IBOutlet var billLabel: UILabel!
+    
+    @IBOutlet weak var tipFindLabel: UILabel!
+    
+    @IBOutlet weak var currencyLabel: UILabel!
+    
+    @IBOutlet weak var buttonText: UIButton!
+    @IBOutlet weak var totalTitle: UILabel!
+    @IBOutlet weak var tipTitle: UILabel!
+    
+    @IBOutlet weak var tipCurrency: UILabel!
+    
+    
+    @IBOutlet weak var totalCurrency: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let locale = Locale.current
+        let currencySymbol = locale.currencySymbol!
+        
+        self.currencyLabel.text = currencySymbol
+        self.totalCurrency.text = currencySymbol
+        self.tipCurrency.text = currencySymbol
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -26,6 +52,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
     @IBAction func changeColor(_ sender: Any) {
         UIView.animate(withDuration: 0.4, animations: {
@@ -42,7 +69,7 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("view will appear")
-
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -74,13 +101,15 @@ class ViewController: UIViewController {
         
     }
     
+    
 
     @IBAction func onTap(_ sender: AnyObject) {
         view.endEditing(true)
     }
     
-
-    @IBAction func CalculateTip(_ sender: AnyObject) {
+    
+    @IBAction func CalculateTip(_ sender: Any) {
+        
         let defaults = UserDefaults.standard
         let doubleValue1 = defaults.double(forKey: "default1")
         let doubleValue2 = defaults.double(forKey: "default2")
@@ -89,21 +118,16 @@ class ViewController: UIViewController {
         let newPercentages = [doubleValue1/100,doubleValue2/100,doubleValue3/100]
 
         
-        
         let bill = Double(billField.text!) ?? 0
         
         let tip = bill * newPercentages[tipControl.selectedSegmentIndex]
         
-        
         let total = bill + tip
         
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
+        tipLabel.text = String(format: "%.2f", tip)
+        totalLabel.text = String(format: "%.2f", total)
         
     }
-    
-    
-    
-    
+
 }
 
